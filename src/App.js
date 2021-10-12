bbd27f32877db5b59daac9ff33a11fd029d1aeb1
Wendel Demetrio <hey.wendel95@gmail.com>
2021-10-12 09:52:07 -0300

    component Post

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import Post from './components/Post'
 
 export default function App() {
   return (
-    <Header />
+    <View style={{ flex: 1 }}>
+      <Header />
+      <Post image={require('../assets/o-que-e-paisagem.jpg')} />
+    </View>
   );
 }
 
