8f8c63ff73ea05298fc8b4e6cf0e34b0270595d6
Wendel Demetrio <hey.wendel95@gmail.com>
2021-10-15 16:05:13 -0300

    conditional rendering to check user's acess

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { NavigationContainer } from '@react-navigation/native'
-
+import { View } from 'react-native'
 import Feed from './pages/Feed'
 import Camera from './pages/Camera'
 import Profile from './pages/Profile'
@@ -10,37 +11,54 @@ import Profile from './pages/Profile'
 
 
 const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator();
+
 
 export default function Navigator() {
+
+  const [loginComplete, setLoginComplete] = useState(false)
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <View style={{ flex: 1 }}>
+      {loginComplete ? (
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'planet'
-                : 'planet-outline';
-            } else if (route.name === 'Feed') {
-              iconName = focused ? 'at-circle' : 'at-circle-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'person-circle' : 'person-circle-outline';
-            }
+                if (route.name === 'Home') {
+                  iconName = focused
+                    ? 'planet'
+                    : 'planet-outline';
+                } else if (route.name === 'Feed') {
+                  iconName = focused ? 'at-circle' : 'at-circle-outline';
+                } else if (route.name === 'Profile') {
+                  iconName = focused ? 'person-circle' : 'person-circle-outline';
+                }
 
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          headerShown: false,
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name='Feed' component={Feed} />
-        <Tab.Screen name='Home' component={Camera} />
-        <Tab.Screen name='Profile' component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              headerShown: false,
+              tabBarActiveTintColor: 'tomato',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
+            <Tab.Screen name='Feed' component={Feed} />
+            <Tab.Screen name='Home' component={Camera} />
+            <Tab.Screen name='Profile' component={Profile} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      ) : (
+        < NavigationContainer >
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+
+          </Stack.Navigator>
+        </NavigationContainer >
+      )}
+    </View>
+
   )
 }
